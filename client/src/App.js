951a11097c7b40c992b199addb66365600b7ba0e
senex127 <antoine127chabernaud@gmail.com>
2024-01-01 20:48:41 +0100

    ajout login

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import Recherche from "./components/Recherche";
 import Recette from "./components/Recette";
+import Login from "./components/Login";
 import logo from "./assets/logo.png";
 
 
@@ -21,12 +22,16 @@ function App() {
             <li>
               <Link to="/recette">Recettes Enregistrées</Link>
             </li>
+            <li>
+              <Link to="/login">Connexion</Link>
+            </li>
           </ul>
         </nav>
 
         <Routes>
             <Route path="/" element={<Recherche />} />
             <Route path="/recette" element={<Recette />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
