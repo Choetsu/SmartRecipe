f65e1ba07046aa0ecc1d156b2531103baa558380
Choetsu <samy.amh94@gmail.com>
2024-01-08 00:34:14 +0100

    fix Favoris

--- client/src/components/Favoris.jsx
+++ client/src/components/Favoris.jsx
@@ -35,25 +35,27 @@ function Favoris() {
                         Mes favoris
                     </h1>
                     <ul className="space-y-4">
-                        {favoris.map((favori) => (
-                            <li
-                                key={favori.id}
-                                className="bg-gray-100 p-4 rounded-lg shadow"
-                            >
-                                <h2 className="text-xl font-semibold text-gray-800 mb-2">
-                                    {favori.name}
-                                </h2>
-                                <p className="text-gray-600 mb-4">
-                                    {favori.description}
-                                </p>
-                                <Link
-                                    to={`/recettes-details/${favori.id}`}
-                                    className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        {favoris &&
+                            favoris.length > 0 &&
+                            favoris.map((favori) => (
+                                <li
+                                    key={favori.id}
+                                    className="bg-gray-100 p-4 rounded-lg shadow"
                                 >
-                                    Voir Détails
-                                </Link>
-                            </li>
-                        ))}
+                                    <h2 className="text-xl font-semibold text-gray-800 mb-2">
+                                        {favori.name}
+                                    </h2>
+                                    <p className="text-gray-600 mb-4">
+                                        {favori.description}
+                                    </p>
+                                    <Link
+                                        to={`/recettes-details/${favori.id}`}
+                                        className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                    >
+                                        Voir Détails
+                                    </Link>
+                                </li>
+                            ))}
                     </ul>
                 </div>
             )}
